b2eaf5ac261060540942d3524bd3c94f0ece8c20
하미르 <mir_lancelot@naver.com>
2022-10-24 15:15:39 +0900

    [back] init : 빈 프로젝트 생성

---
+++ back_footp/src/main/java/com/ssafy/back_footp/BackFootpApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.back_footp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackFootpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackFootpApplication.class, args);
+	}
+
+}
