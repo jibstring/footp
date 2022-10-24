2ebceece4ff6a9f5ceb9b4274cc7300089a716b6
하미르 <mir_lancelot@naver.com>
2022-10-25 08:45:55 +0900

    [BE/entity] ⚙️refactor : 폴더구조 수정

--- back_footp/src/main/java/com/ssafy/back_footp/entity/Event.java
+++ back_footp/src/main/java/com/ssafy/back_footp/entity/Event.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.locationtech.jts.geom.Point;
 import org.springframework.data.annotation.CreatedDate;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
