154644be694d0674e8a0d160d7a01d5cfeeab5e3
김태웅 <85680155+KimTaeWonng@users.noreply.github.com>
2022-10-27 13:26:59 +0900

    [BE/JPAmain] 🛠️fix : fix red line
    221027 TWK

--- back_footp/src/main/java/com/ssafy/back_footp/config/SwaggerConfig.java
+++ back_footp/src/main/java/com/ssafy/back_footp/config/SwaggerConfig.java
@@ -17,7 +17,7 @@ public class SwaggerConfig {
 
 	@Bean
 	public Docket restAPI() {
-		return new Docket(DocumentationType.OAS_30)
+		return new Docket(DocumentationType.SWAGGER_2)
 				.useDefaultResponseMessages(false)
 				.select()
 				.apis(RequestHandlerSelectors.basePackage("com.ssafy.back_footp.controller"))
