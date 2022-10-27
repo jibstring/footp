5b215f5ae71bf4b7d0cd2d847e6695dec4784d56
kyong0409 <kyong0409@nate.com>
2022-10-27 09:56:24 +0900

    [App] feat: add tab for creating foot

--- app_footp/lib/createFoot.dart
+++ app_footp/lib/createFoot.dart
@@ -1,15 +1,71 @@
 import 'package:flutter/material.dart';
 
-class createFoot extends StatelessWidget{
-  const createFoot({Key? key}):super(key:key);
+const List<Widget> types = <Widget>[Text('일반'), Text('이벤트')];
 
-  Widget build(BuildContext context){
-    return Scaffold(
-      body:Center(
-        child: Text('글쓰기')
-        ),
+class CreateFoot extends StatelessWidget {
+  const CreateFoot({super.key});
+
+  static const String _title = 'ToggleButtons Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: _title,
+      home: ToggleButtonsSample(title: _title),
     );
   }
-
 }
 
+class ToggleButtonsSample extends StatefulWidget {
+  const ToggleButtonsSample({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<ToggleButtonsSample> createState() => _ToggleButtonsSampleState();
+}
+
+class _ToggleButtonsSampleState extends State<ToggleButtonsSample> {
+  final List<bool> _selectedTypes = <bool>[true, false];
+  bool vertical = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // ToggleButtons with a single selection.
+            const SizedBox(height: 5),
+            ToggleButtons(
+              direction: vertical ? Axis.vertical : Axis.horizontal,
+              onPressed: (int index) {
+                setState(() {
+                  // The button that is tapped is set to true, and the others to false.
+                  for (int i = 0; i < _selectedTypes.length; i++) {
+                    _selectedTypes[i] = i == index;
+                  }
+                });
+              },
+              borderRadius: const BorderRadius.all(Radius.circular(8)),
+              selectedBorderColor: Colors.indigo[100],
+              selectedColor: Colors.black,
+              fillColor: Colors.indigo[100],
+              color: Colors.black45,
+              constraints: const BoxConstraints(
+                minHeight: 30.0,
+                minWidth: 80.0,
+              ),
+              isSelected: _selectedTypes,
+              children: types,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- app_footp/lib/mainMap.dart
+++ app_footp/lib/mainMap.dart
@@ -1,39 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:app_footp/createFoot.dart';
 
-class mainMap extends StatelessWidget{
-  const mainMap({Key? key}):super(key:key);
+class mainMap extends StatelessWidget {
+  const mainMap({Key? key}) : super(key: key);
 
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text("메인페이지"),
-        actions:<Widget>[
+        title: Text("메인페이지"),
+        actions: <Widget>[
           IconButton(
-            icon:Icon(
+            icon: Icon(
               Icons.add,
-              color:Colors.black,
+              color: Colors.black,
             ),
-            onPressed:(){},
-           ),
+            onPressed: () {},
+          ),
         ],
       ),
-      body :Center(
-        child: Column(
-          mainAxisAlignment:MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child:Text(""),
-              onPressed:(){
-                Navigator.push(context,MaterialPageRoute(builder:(context)=>createFoot()),
+      body: Center(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          ElevatedButton(
+              child: Text(""),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => CreateFoot()),
                 );
               },
-              style:ElevatedButton.styleFrom(
-                primary:Colors.red
-              )),
-          ]),
-        ),
+              style: ElevatedButton.styleFrom(primary: Colors.red)),
+        ]),
+      ),
     );
   }
-
 }
