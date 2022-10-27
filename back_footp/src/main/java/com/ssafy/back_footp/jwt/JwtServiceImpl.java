ab09b035235e5a7c90743620596da12b17c81d9c
김태웅 <85680155+KimTaeWonng@users.noreply.github.com>
2022-10-27 15:50:16 +0900

    [BE/MessageTW] ⚡feat : Jwt basic
    221027 JWT 기본 세팅 끝

---
+++ back_footp/src/main/java/com/ssafy/back_footp/jwt/JwtServiceImpl.java
@@ -0,0 +1,101 @@
+package com.ssafy.back_footp.jwt;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+public class JwtServiceImpl implements JwtService{
+	
+	public static final Logger logger = LoggerFactory.getLogger(JwtServiceImpl.class);
+
+	private static final String SALT = "E4jb#jc;s$if422jjDFEFJJ33jJEFdd#kd";
+	private static final int EXPIRE_MINUTES = 60;
+
+
+	@Override
+	public <T> String create(String key, T data, String subject) {
+		String jwt = Jwts.builder().setHeaderParam("typ", "JWT").setHeaderParam("regDate", System.currentTimeMillis())
+				.setExpiration(new Date(System.currentTimeMillis()+1000*60*EXPIRE_MINUTES)).setSubject(subject)
+				.claim(key, data).signWith(SignatureAlgorithm.HS256, this.generateKey()).compact();
+		return jwt;
+	}
+
+	private byte[] generateKey() {
+		byte[] key = null;
+		try {
+			key = SALT.getBytes("UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			if (logger.isInfoEnabled()) {
+				e.printStackTrace();
+			} else {
+				logger.error("Making JWT Key Error ::: {}", e.getMessage());
+			}
+		}
+
+		return key;
+
+	}
+
+	@Override
+	public Map<String, Object> get() {
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+				.getRequest();
+		String jwt = request.getHeader("Authorization");
+		Jws<Claims> claims = null;
+		try {
+			claims = Jwts.parser().setSigningKey(SALT.getBytes("UTF-8")).parseClaimsJws(jwt);
+		} catch (Exception e) {
+//			if (logger.isInfoEnabled()) {
+//				e.printStackTrace();
+//			} else {
+			logger.error(e.getMessage());
+//			}
+			throw new UnauthorizedException();
+//			개발환경
+//			Map<String,Object> testMap = new HashMap<>();
+//			testMap.put("userid", userid);
+//			return testMap;
+		}
+		Map<String, Object> value = claims.getBody();
+		logger.info("value : {}", value);
+		return value;
+
+	}
+
+	@Override
+	public int getUserId() {
+		return (int) this.get().get("userid");
+	}
+
+	@Override
+	public boolean isUsable(String jwt) {
+		try {
+			Jws<Claims> claims = Jwts.parser().setSigningKey(this.generateKey()).parseClaimsJws(jwt);
+			return true;
+		} catch (Exception e) {
+//			if (logger.isInfoEnabled()) {
+//				e.printStackTrace();
+//			} else {
+			logger.error(e.getMessage());
+//			}
+//			throw new UnauthorizedException();
+//			개발환경
+			return false;
+		}
+
+	}
+
+
+}
