eeefb5d4959a8187dec2f284fbc9930a29bac1cd
kyong0409 <kyong0409@nate.com>
2022-10-27 14:46:14 +0900

    [App] feat: add radio button to normal foot

--- app_footp/lib/components/createFoot/normalForm.dart
+++ app_footp/lib/components/createFoot/normalForm.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -9,9 +10,50 @@ class NormalForm extends StatefulWidget {
   State<NormalForm> createState() => _NormalFormState();
 }
 
+enum OpenRange { all, me }
+
 class _NormalFormState extends State<NormalForm> {
+  OpenRange _openRange = OpenRange.all;
+
   @override
   Widget build(BuildContext context) {
-    return Text('노말이면 공개범위 선택');
+    return Container(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Text(
+          '공개 범위',
+          style: TextStyle(fontSize: 15),
+          textAlign: TextAlign.start,
+        ),
+        Row(children: <Widget>[
+          Expanded(
+            child: RadioListTile(
+              title: Text('전체 공개'),
+              value: OpenRange.all,
+              groupValue: _openRange,
+              onChanged: (value) {
+                setState(() {
+                  _openRange = value!;
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: RadioListTile(
+              title: Text('나만 보기'),
+              value: OpenRange.me,
+              groupValue: _openRange,
+              onChanged: (value) {
+                setState(() {
+                  _openRange = value!;
+                });
+              },
+            ),
+          )
+        ]),
+      ],
+    ));
   }
 }
