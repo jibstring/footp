9ea33cf46e206630bec2073d109e6e196ab891c2
김태웅 <85680155+KimTaeWonng@users.noreply.github.com>
2022-10-27 17:59:21 +0900

    [BE/Mail] ⚡feat : Mail
    221027 메일 인증 보내기 ( 오류남 수정 필수)

---
+++ back_footp/src/main/java/com/ssafy/back_footp/config/MailConfig.java
@@ -0,0 +1,41 @@
+package com.ssafy.back_footp.config;
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+
+@Configuration
+public class MailConfig {
+
+	@Bean
+	public JavaMailSender javaMailService() {
+		JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+		
+		javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setUsername("apxjvm@gmail.com");
+        javaMailSender.setPassword("lhrszjommjqhuvrp"); //이제 구글 보안은 이 앱 비밀번호를 사용해서 해야합니다. (2단계 보안 설정 후 앱 비밀번호를 발급받아 사용)
+
+        javaMailSender.setPort(465);
+
+        javaMailSender.setJavaMailProperties(getMailProperties());
+
+        return javaMailSender;
+        
+        
+	}
+
+	private Properties getMailProperties() {
+		Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.debug", "true");
+        properties.setProperty("mail.smtp.ssl.trust","smtp.gmail.com");
+        properties.setProperty("mail.smtp.ssl.enable","true");
+        return properties;
+	}
+}
---
+++ back_footp/src/main/java/com/ssafy/back_footp/service/MailService.java
@@ -0,0 +1,47 @@
+package com.ssafy.back_footp.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.ssafy.back_footp.entity.Mail;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MailService {
+
+	@Autowired
+	private MailSender MailSender;
+
+	public void mailSend(Mail mail) {
+
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(mail.getAddress());
+		message.setFrom("apxjvm@gmail.com");
+		message.setSubject(mail.getTitle());
+		message.setText(mail.getContent());
+
+		MailSender.send(message);
+	}
+
+	public Mail sendEmailService(String email, String name) {
+		try {
+			Mail mail = new Mail();
+
+			mail.setAddress(email);
+			mail.setTitle(name);
+			mail.setContent("test");
+
+			return mail;
+		} catch (Exception e) {
+			return null;
+		}
+	}
+}
