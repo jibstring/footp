bac83df1fb37ee42f0216e9e359925499c747ab0
aspyo3 <aspyo3@naver.com>
2022-10-27 14:54:45 +0900

    [FE/mainMap] ⚡feat : make main component

--- app_footp/lib/mainMap.dart
+++ app_footp/lib/mainMap.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:naver_map_plugin/naver_map_plugin.dart';
+import 'package:app_footp/createFoot.dart';
 
 void main() {
   runApp(const mainMap());
@@ -15,16 +16,8 @@ class mainMap extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pootf',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Footp Main Page'),
@@ -35,15 +28,6 @@ class mainMap extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -53,7 +37,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
 
+
+  //목록 
   static List<Widget> widgetOptions = <Widget>[
+    //발자국 목록
     DraggableScrollableSheet(
       initialChildSize: 0.3,
       minChildSize: 0.2,
@@ -71,6 +58,7 @@ class _MyHomePageState extends State<MyHomePage> {
         );
       },
     ),
+    //채팅 방
     DraggableScrollableSheet(
       initialChildSize: 0.3,
       minChildSize: 0.2,
@@ -101,13 +89,24 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
+      appBar: AppBar(
+        title:Image.asset('imgs/logo.png',height:45),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        actions:<Widget>[
+          IconButton(
+            icon:Icon(
+              Icons.account_circle,
+              color:Color.fromARGB(255, 153, 181, 229),
+              size: 40,
+            ),
+            padding:const EdgeInsets.only(top:5,right:20.0),
+            onPressed:(){},
+           ),
+        ],
+      ),
       body: SizedBox.expand(
           child: Stack(children: <Widget>[
         NaverMap(
@@ -140,3 +139,4 @@ class _MyHomePageState extends State<MyHomePage> {
     _controller.complete(controller);
   }
 }
+
