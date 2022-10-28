7e474c2d341265e42e1f35ec4785837aeb1ecab3
kyong0409 <kyong0409@nate.com>
2022-10-28 16:16:30 +0900

    [App] feat: create foot add files(in progress)

--- app_footp/lib/components/createFoot/normalForm.dart
+++ app_footp/lib/components/createFoot/normalForm.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:file_picker/file_picker.dart';
+import 'dart:typed_data';
+import 'package:dio/dio.dart';
 
 class NormalForm extends StatefulWidget {
   const NormalForm({super.key});
@@ -10,10 +13,32 @@ class NormalForm extends StatefulWidget {
   State<NormalForm> createState() => _NormalFormState();
 }
 
+Future<dynamic> patchFootData(dynamic input) async {
+  print("일반 게시글을 작성합니다.");
+  var dio = new Dio();
+  try {
+    dio.options.contentType = 'multipart/form-data';
+
+    print("############data: $input");
+
+    // var response = await dio.patch(
+    //   baseUri + '/users/profileimage',
+    //   data: input,
+    // );
+    print('성공적으로 업로드했습니다');
+  } catch (e) {
+    print(e);
+  }
+}
+
 enum OpenRange { all, me }
 
 class _NormalFormState extends State<NormalForm> {
   OpenRange _openRange = OpenRange.all;
+  String showFileName = "";
+  List<String> allowedFileTypes = ['jpg', 'mp4', 'txt', 'pdf'];
+  final myText = TextEditingController();
+  FilePickerResult? result;
 
   @override
   Widget build(BuildContext context) {
@@ -22,6 +47,88 @@ class _NormalFormState extends State<NormalForm> {
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       mainAxisSize: MainAxisSize.min,
       children: <Widget>[
+        TextField(
+          maxLines: 10,
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(),
+            alignLabelWithHint: true,
+            hintText: '메세지를 입력하세요',
+          ),
+          controller: myText,
+        ),
+        Container(
+          height: 200,
+          width: 400,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 5,
+              color: Colors.grey,
+            ),
+            borderRadius: const BorderRadius.all(Radius.circular(20)),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              InkWell(
+                onTap: () async {
+                  FilePickerResult? result =
+                      await FilePicker.platform.pickFiles(
+                    type: FileType.custom,
+                    allowedExtensions: allowedFileTypes,
+                  );
+                  if (result != null) {
+                    print('########## ${result.files.first}');
+                    PlatformFile file = result.files.single;
+                    String fileName = result.files.first.name;
+                    // Uint8List fileBytes = result.files.first.bytes!;
+                    debugPrint(fileName);
+                    print('name: ${file.path}');
+                    // var multipartFile = await MultipartFile.fromFile(
+                    //   file.path,
+                    // );
+                    setState(() {
+                      showFileName = "Now File Name: $fileName";
+                    });
+                  }
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      "Find and Upload",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                        fontSize: 20,
+                      ),
+                    ),
+                    Icon(
+                      Icons.upload_rounded,
+                      color: Colors.grey,
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                "$allowedFileTypes",
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                showFileName,
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ),
         Text(
           '공개 범위',
           style: TextStyle(fontSize: 15),
@@ -51,8 +158,24 @@ class _NormalFormState extends State<NormalForm> {
                 });
               },
             ),
-          )
+          ),
         ]),
+        Container(
+            child: IconButton(
+                onPressed: () {
+                  var formData = FormData.fromMap({
+                    'messageText': myText.text,
+                    'messageFileurl': result?.files.first.name ?? '',
+                    'messageLongtitude': 37.60251338193296,
+                    'messageLatitude': 127.12306290392186,
+                    'isOpentoall': _openRange == OpenRange.all ? true : false,
+                  });
+                  print(formData.fields);
+                },
+                icon: Icon(
+                  Icons.handshake,
+                  size: 24,
+                ))),
       ],
     ));
   }
--- app_footp/lib/components/createFoot/quizForm.dart
+++ app_footp/lib/components/createFoot/quizForm.dart
@@ -5,6 +5,8 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:numberpicker/numberpicker.dart';
 import 'package:dio/dio.dart';
+import 'package:file_picker/file_picker.dart';
+import 'dart:typed_data';
 
 class QuizForm extends StatefulWidget {
   const QuizForm({super.key});
@@ -17,6 +19,9 @@ class _QuizFormState extends State<QuizForm> {
   int _currentValue = 0;
   final int _maxValue = 24;
   final int _minValue = 0;
+  bool _isChecked = false;
+  String showFileName = "";
+  List<String> allowedFileTypes = ['jpg', 'mp4', 'txt', 'pdf'];
 
   void getHttp() async {
     try {
@@ -31,6 +36,93 @@ class _QuizFormState extends State<QuizForm> {
   Widget build(BuildContext context) {
     return Container(
         child: Column(children: <Widget>[
+      TextField(
+        maxLines: 10,
+        decoration: InputDecoration(
+          border: const OutlineInputBorder(),
+          alignLabelWithHint: true,
+          hintText: '메세지를 입력하세요',
+        ),
+      ),
+      Container(
+        height: 200,
+        width: 400,
+        decoration: BoxDecoration(
+          border: Border.all(
+            width: 5,
+            color: Colors.grey,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(20)),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              onTap: () async {
+                FilePickerResult? result = await FilePicker.platform.pickFiles(
+                  type: FileType.custom,
+                  allowedExtensions: allowedFileTypes,
+                );
+                if (result != null && result.files.isNotEmpty) {
+                  String fileName = result.files.first.name;
+                  Uint8List fileBytes = result.files.first.bytes!;
+                  debugPrint(fileName);
+                  setState(() {
+                    showFileName = "Now File Name: $fileName";
+                  });
+                  /*
+                do jobs
+                 */
+                }
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    "Find and Upload",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey,
+                      fontSize: 20,
+                    ),
+                  ),
+                  Icon(
+                    Icons.upload_rounded,
+                    color: Colors.grey,
+                  ),
+                ],
+              ),
+            ),
+            Text(
+              "$allowedFileTypes",
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              showFileName,
+              style: TextStyle(
+                color: Colors.grey,
+              ),
+            ),
+          ],
+        ),
+      ),
+      Container(
+          child: CheckboxListTile(
+        title: Text('퀴즈 내기'),
+        value: _isChecked,
+        onChanged: (value) {
+          setState(() {
+            _isChecked = value!;
+          });
+        },
+      )),
       Container(
           child: Column(
         children: <Widget>[
@@ -71,83 +163,98 @@ class _QuizFormState extends State<QuizForm> {
           )
         ],
       )),
-      Container(
-        decoration: BoxDecoration(
-            border: Border.all(), color: Color.fromARGB(255, 194, 223, 237)),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Text(
-              '퀴즈',
-              style: TextStyle(fontSize: 15),
+      Column(
+        children: <Widget>[
+          if (_isChecked == true)
+            Container(
+              decoration: BoxDecoration(
+                  border: Border.all(),
+                  color: Color.fromARGB(255, 194, 223, 237)),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text(
+                    '퀴즈',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  Text(
+                    '문제',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), hintText: '문제를 입력하세요'),
+                    maxLines: 5,
+                  ),
+                  Text(
+                    '정답',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(), hintText: '정답을 입력하세요'),
+                  ),
+                  const Divider(
+                    color: Colors.grey,
+                    height: 20,
+                    thickness: 1,
+                    indent: 20,
+                    endIndent: 0,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        border: Border.all(),
+                        color: Color.fromARGB(255, 194, 223, 237)),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Text(
+                          '해설',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        Text(
+                          '글',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        TextField(
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: '해설을 입력하세요'),
+                          maxLines: 5,
+                        ),
+                        Text(
+                          '이미지',
+                          textAlign: TextAlign.left,
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        TextButton(
+                            onPressed: () async {
+                              FilePickerResult? result =
+                                  await FilePicker.platform.pickFiles();
+                              if (result == null) {
+                                print("No file selected");
+                              } else {
+                                print(result.files.single.name);
+                              }
+                            },
+                            child: Text('버튼'))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-            Text(
-              '문제',
-              style: TextStyle(fontSize: 15),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), hintText: '문제를 입력하세요'),
-              maxLines: 5,
-            ),
-            Text(
-              '정답',
-              style: TextStyle(fontSize: 15),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), hintText: '정답을 입력하세요'),
-            ),
-          ],
-        ),
-      ),
-      const Divider(
-        color: Colors.grey,
-        height: 20,
-        thickness: 1,
-        indent: 20,
-        endIndent: 0,
-      ),
-      Container(
-        decoration: BoxDecoration(
-            border: Border.all(), color: Color.fromARGB(255, 194, 223, 237)),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Text(
-              '해설',
-              textAlign: TextAlign.left,
-              style: TextStyle(fontSize: 15),
-            ),
-            Text(
-              '글',
-              textAlign: TextAlign.left,
-              style: TextStyle(fontSize: 15),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(), hintText: '해설을 입력하세요'),
-              maxLines: 5,
-            ),
-            Text(
-              '이미지',
-              textAlign: TextAlign.left,
-              style: TextStyle(fontSize: 15),
-            ),
-            TextButton(
-                onPressed: () async {
-                  FilePickerResult? result =
-                      await FilePicker.platform.pickFiles();
-                  if (result == null) {
-                    print("No file selected");
-                  } else {
-                    print(result.files.single.name);
-                  }
-                },
-                child: Text('버튼'))
-          ],
-        ),
-      ),
+          Container(
+              child: IconButton(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.handshake,
+                    size: 24,
+                  ))),
+        ],
+      )
     ]));
   }
 }
