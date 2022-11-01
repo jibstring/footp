ffdfcb9aa77f8387da72b996f833a737675c6373
김태웅 <85680155+KimTaeWonng@users.noreply.github.com>
2022-11-01 15:33:46 +0900

    [BE/MailTW] ⚡feat : pw code reset
    221101 TWK 이메일 코드발송 (비번 재설정)

--- back_footp/src/main/java/com/ssafy/back_footp/controller/AuthController.java
+++ back_footp/src/main/java/com/ssafy/back_footp/controller/AuthController.java
@@ -43,7 +43,7 @@ public class AuthController {
 
 	@Autowired
 	MailService mailService;
-	
+
 	@Autowired
 	JwtService jwtService;
 
@@ -63,7 +63,7 @@ public class AuthController {
 				.build();
 
 		try {
-			//이미 등록된 이메일이 아니라면
+			// 이미 등록된 이메일이 아니라면
 			if (!authService.emailCheck(userEntity.getUserEmail())) {
 				authService.createUser(userEntity);
 				String token = jwtService.create("user_id", user.getUserId(), "Authorization");
@@ -80,35 +80,36 @@ public class AuthController {
 
 		return new ResponseEntity<Map<String, Object>>(result, HttpStatus.OK);
 	}
-	
-	@PostMapping("signin")
+
+	@PostMapping("/signin")
 	@ApiOperation(value = "로그인")
-	public ResponseEntity<Map<String,Object>> signIn(@RequestBody @ApiParam(value = "이메일, 비밀번호로 로그인",required = true) User user) throws Exception{
+	public ResponseEntity<Map<String, Object>> signIn(
+			@RequestBody @ApiParam(value = "이메일, 비밀번호로 로그인", required = true) User user) throws Exception {
 		Map<String, Object> result = new HashMap<>();
-		
+
 		HttpStatus status = null;
 		try {
 			User loginUser = authService.login(user.getUserEmail(), user.getUserPassword());
-			
-			if(loginUser!=null) {
+
+			if (loginUser != null) {
 				String token = jwtService.create("userid", loginUser.getUserId(), "Authorization");
-				
-				logger.debug("로그인 토큰 : {}",token);
+
+				logger.debug("로그인 토큰 : {}", token);
 				result.put("Authorization", token);
 				result.put("message", SUCCESS);
 				status = HttpStatus.ACCEPTED;
-			}else {
+			} else {
 				result.put("message", FAIL);
 				status = HttpStatus.ACCEPTED;
 			}
 		} catch (Exception e) {
 			// TODO: handle exception
-			logger.error("로그인 에러 : {}",e);
+			logger.error("로그인 에러 : {}", e);
 			result.put("message", e.getMessage());
 			status = HttpStatus.INTERNAL_SERVER_ERROR;
 		}
-		
-		return new ResponseEntity<Map<String,Object>>(result,status);
+
+		return new ResponseEntity<Map<String, Object>>(result, status);
 	}
 
 	@PostMapping("/duplicate/{email}")
@@ -130,44 +131,100 @@ public class AuthController {
 		return new ResponseEntity<Boolean>(check, HttpStatus.OK);
 
 	}
-	
+
 	@PostMapping("/email/{email}")
 	@ApiOperation(value = "해당 이메일의 회원 정보가 있으면 인증메일을 보냄(가입용)")
-	public ResponseEntity<Integer> codeMailSend(@PathVariable String email){
-		
+	public ResponseEntity<Integer> codeMailSend(@PathVariable String email) {
+
 		int result = 1;
-		
+
 		User user = userRepository.findByUserEmail(email).get();
-		
-		//이미 인증된 계정이거나, 존재하지 않는 계정이면 패스
-		if(authService.emailCheck(email) && !user.getUserEmailKey().equals("Y")) {
-			
+
+		// 이미 인증된 계정이거나, 존재하지 않는 계정이면 패스
+		if (authService.emailCheck(email) && !user.getUserEmailKey().equals("Y")) {
+
 			Mail mail = authService.sendEmailServiceForSignUp(email, "회원");
 			System.out.println("메일이 잘 보내지나요");
 			mailService.mailSend(mail);
 			System.out.println("메일이 잘 보내지네요");
-		}else {
+		} else {
 			result = 0;
 		}
-		
-		return new ResponseEntity<Integer>(result,HttpStatus.OK);
+
+		return new ResponseEntity<Integer>(result, HttpStatus.OK);
 	}
-	
+
 	@PostMapping("/success/{userId}/{code}")
 	@ApiOperation(value = "회원가입 후 이메일 인증")
-	public ResponseEntity<Boolean> codeCheck(@PathVariable String code, @PathVariable long userId){
+	public ResponseEntity<Boolean> codeCheck(@PathVariable String code, @PathVariable long userId) {
 		boolean result = false;
 		User user = userRepository.findByUserId(userId);
-		
-		//코드가 일치하고, 3분 기한 내에 입력하였을 경우
-		if(user.getUserEmailKey().equals(code) && user.getUserPwfindtime().isAfter(LocalDateTime.now())) {
+
+		// 코드가 일치하고, 3분 기한 내에 입력하였을 경우
+		if (user.getUserEmailKey().equals(code) && user.getUserPwfindtime().isAfter(LocalDateTime.now())) {
 			user.setUserEmailKey("Y");
-			
+
 			userRepository.save(user);
 			result = true;
 		}
+
+		return new ResponseEntity<Boolean>(result, HttpStatus.OK);
+	}
+
+	@PostMapping("/emailPW/{email}")
+	@ApiOperation(value = "비밀번호 재설정 용 이메일 인증코드")
+	public ResponseEntity<Integer> passwordMailSend(@PathVariable String email) {
+		int result = 1;
+
+		User user = userRepository.findByUserEmail(email).get();
+
+		// 이미 인증된 계정이거나, 존재하지 않는 계정이면 패스
+		if (authService.emailCheck(email)) {
+
+			Mail mail = authService.sendEmailServiceForPassword(email, user.getUserNickname());
+			System.out.println("메일이 잘 보내지나요");
+			mailService.mailSend(mail);
+			System.out.println("메일이 잘 보내지네요");
+		} else {
+			result = 0;
+		}
+
+		return new ResponseEntity<Integer>(result, HttpStatus.OK);
+	}
+	
+	@PostMapping("/validate/{userId}/{code}")
+	@ApiOperation(value = "재설정 목적의 이메일 코드 인증")
+	public ResponseEntity<Boolean> codeCheckPassword(@PathVariable String code, @PathVariable long userId) {
+		boolean result = false;
+		User user = userRepository.findByUserId(userId);
+
+		// 코드가 일치하고, 3분 기한 내에 입력하였을 경우
+		if (user.getUserPwfindkey().equals(code) && user.getUserPwfindtime().isAfter(LocalDateTime.now())) {
+			result = true;
+		}
+
+		return new ResponseEntity<Boolean>(result, HttpStatus.OK);
+	}
+	
+	@PostMapping("/lostPW/{userId}/{password}")
+	@ApiOperation(value = "재설정 목적의 이메일 코드 인증 후 비밀번호 재설정")
+	public ResponseEntity<Integer> resetPassword(@PathVariable long userId, @PathVariable String password) {
+		int result = 0;
 		
-		return new ResponseEntity<Boolean>(result,HttpStatus.OK);
+		User user = userRepository.findByUserId(userId);
+
+		// 코드가 일치하고, 3분 기한 내에 입력하였을 경우
+		try {
+			user.setUserPassword(EncryptionUtils.encryptSHA256(password));
+			userRepository.save(user);
+			
+			result = 1;
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+
+		return new ResponseEntity<Integer>(result, HttpStatus.OK);
 	}
 
 }
