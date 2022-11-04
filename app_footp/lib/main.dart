f93e5b0e5d243c72059cdbc5878ba40a81b13c84
ehfkd5000 <ehfkd5000@naver.com>
2022-11-04 16:25:38 +0900

    ⚡feat : jwt 디코딩

--- app_footp/lib/main.dart
+++ app_footp/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:app_footp/singIn.dart';
 import 'package:flutter/material.dart';
 import 'package:app_footp/mainMap.dart';
 
@@ -9,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: mainMap(),
+      home: SignIn(),
     );
   }
 }
