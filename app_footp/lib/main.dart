8ef6996bdfad402adb35a9e9c1359934e45201f2
aspyo3 <aspyo3@naver.com>
2022-11-04 16:04:50 +0900

    [FE/signup] ⚡feat : add nickname input

--- app_footp/lib/main.dart
+++ app_footp/lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:app_footp/signUp.dart';
+import 'package:app_footp/singIn.dart';
 import 'package:flutter/material.dart';
 import 'package:app_footp/mainMap.dart';
 
@@ -9,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: mainMap(),
+      home: SignUp(),
     );
   }
 }
