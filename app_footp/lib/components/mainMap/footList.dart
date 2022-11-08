d245774efd033daa0fef98b5346ab7b859890fd7
김기태 <lop246@naver.com>
2022-11-08 16:33:49 +0900

    [FE/footList] 🛠️fix : change list view sheet

--- app_footp/lib/components/mainMap/footList.dart
+++ app_footp/lib/components/mainMap/footList.dart
@@ -69,52 +69,56 @@ class _FootListState extends State<FootList> {
             color: Colors.white,
             child: ListView.builder(
                 controller: scrollController,
-                itemCount: eventlen + messagelen + 1,
+                itemCount: eventlen + messagelen + 2,
                 itemBuilder: (BuildContext context, int index) {
-                  return (index == 0)
-                      ? Container(
-                          color: Colors.white,
-                          height: 50,
-                          padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              // 필터
-                              DropdownButton(
-                                value: _selectedValue,
-                                items: _valueList.map(
-                                  (value) {
-                                    return DropdownMenuItem(
-                                        value: value, child: Text(value));
-                                  },
-                                ).toList(),
-                                onChanged: (value) {
-                                  setState(() {
-                                    _selectedValue = value!;
-                                  });
-                                },
-                              ),
-                              // 새로고침
-                              IconButton(
-                                icon: Icon(
-                                  Icons.refresh,
-                                  size: 40,
-                                ),
-                                onPressed: () {
-                                  readFile();
-                                },
-                              ),
-                              IconButton(
-                                // 검색
-                                onPressed: () {},
-                                icon: Icon(Icons.search, size: 40),
-                              ),
-                            ],
+                  if (index == 0) {
+                    return Container(
+                      color: Colors.white,
+                      height: 50,
+                      padding: EdgeInsets.fromLTRB(15, 0, 15, 0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          // 필터
+                          DropdownButton(
+                            value: _selectedValue,
+                            items: _valueList.map(
+                              (value) {
+                                return DropdownMenuItem(
+                                    value: value, child: Text(value));
+                              },
+                            ).toList(),
+                            onChanged: (value) {
+                              setState(() {
+                                _selectedValue = value!;
+                              });
+                            },
                           ),
-                        )
-                      : ((footData[index]["check"] == 0)
-                          ? EventFoot(footData[index])
-                          : NormalFoot(footData[index]));
+                          // 새로고침
+                          IconButton(
+                            icon: Icon(
+                              Icons.refresh,
+                              size: 40,
+                            ),
+                            onPressed: () {
+                              readFile();
+                            },
+                          ),
+                          IconButton(
+                            // 검색
+                            onPressed: () {},
+                            icon: Icon(Icons.search, size: 40),
+                          ),
+                        ],
+                      ),
+                    );
+                  } else if (index == eventlen + messagelen + 1) {
+                    return Container(color: Colors.white, height: 60);
+                  } else {
+                    return (footData[index]["check"] == 0)
+                        ? EventFoot(footData[index])
+                        : NormalFoot(footData[index]);
+                  }
                 }));
       },
     );
