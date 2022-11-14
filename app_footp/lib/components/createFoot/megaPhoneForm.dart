2fa3a8a4bef6f419b201068d739ee7433dd36bc9
ehfkd5000 <ehfkd5000@naver.com>
2022-11-14 13:19:19 +0900

    ⚡feat : 시간 설정

--- app_footp/lib/components/createFoot/megaPhoneForm.dart
+++ app_footp/lib/components/createFoot/megaPhoneForm.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: unnecessary_this
 
+import 'package:app_footp/createFoot.dart';
 import 'package:app_footp/createFootMap.dart';
 import 'package:app_footp/main.dart';
 import 'package:app_footp/myLocation.dart';
@@ -31,6 +32,115 @@ class _megaPhoneFormState extends State<megaPhoneForm> {
   FilePickerResult? result;
   String? filePath = '';
   UserData user = Get.put(UserData());
+  var _timeresult = "종료 시간 설정";
+  var _timeresultindex;
+
+  addtime() {
+    final today = DateTime.now();
+    var addDate;
+    switch (_timeresultindex) {
+      case 0:
+        {
+          addDate = today.add(const Duration(hours: 1));
+          return addDate.toString();
+        }
+      case 1:
+        {
+          addDate = today.add(const Duration(hours: 2));
+          return addDate.toString();
+        }
+      case 2:
+        {
+          addDate = today.add(const Duration(hours: 3));
+          return addDate.toString();
+        }
+      case 3:
+        {
+          addDate = today.add(const Duration(hours: 6));
+          return addDate.toString();
+        }
+      case 4:
+        {
+          addDate = today.add(const Duration(hours: 12));
+          return addDate.toString();
+        }
+      case 5:
+        {
+          addDate = today.add(const Duration(days: 1));
+          return addDate.toString();
+        }
+      case 6:
+        {
+          addDate = today.add(const Duration(days: 2));
+          return addDate.toString();
+        }
+      case 7:
+        {
+          addDate = today.add(const Duration(days: 7));
+          return addDate.toString();
+        }
+      case 8:
+        {
+          addDate = today.add(const Duration(days: 36500));
+          return addDate.toString();
+        }
+    }
+  }
+
+  Future<void> _neverSatisfied(BuildContext context) async {
+    final _items = [
+      "1시간",
+      "2시간",
+      "3시간",
+      "6시간",
+      "12시간",
+      "하루",
+      "이틀",
+      "일주일",
+      "평생"
+    ];
+    var result = _items[0]; // 기본값 0
+    return showDialog<void>(
+        context: context,
+        // 사용자가 다이얼로그 바깥을 터치하면 닫히지 않음
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("시간 설정"),
+            content: SingleChildScrollView(
+              child: Container(
+                height: 150.0,
+                child: CupertinoPicker(
+                  children: _items.map((e) => Text('$e')).toList(),
+                  itemExtent: 40.0,
+                  onSelectedItemChanged: (int value) {
+                    result = _items[value];
+                    setState(() {
+                      _timeresult = result;
+                      _timeresultindex = value;
+                    });
+                  },
+                ),
+              ),
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: const Text('확인'),
+                onPressed: () {
+                  // 다이얼로그 닫기
+                  Navigator.of(context).pop();
+                },
+              ),
+              // TextButton(
+              //     onPressed: () {
+              //       // 다이얼로그 닫기
+              //       Navigator.of(context).pop();
+              //     },
+              //     child: const Text('취소'))
+            ],
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +154,7 @@ class _megaPhoneFormState extends State<megaPhoneForm> {
       children: <Widget>[
         SizedBox(height: 40),
         TextField(
-          maxLines: 10,
+          maxLines: 8,
           decoration: InputDecoration(
             border: const OutlineInputBorder(
               borderRadius: const BorderRadius.all(Radius.circular(10)),
@@ -55,7 +165,7 @@ class _megaPhoneFormState extends State<megaPhoneForm> {
           controller: myMegaText,
         ),
         Container(
-          height: 200,
+          height: 150,
           width: 400,
           decoration: BoxDecoration(
             border: Border.all(
@@ -143,7 +253,14 @@ class _megaPhoneFormState extends State<megaPhoneForm> {
             ],
           ),
         ),
-        SizedBox(height: 30),
+        // SizedBox(height: 50),
+        CupertinoButton(
+          child: Text("$_timeresult"),
+          onPressed: () {
+            _neverSatisfied(context);
+          },
+        ),
+        // Text("$_timeresult"),
         Container(
             child: IconButton(
           onPressed: () async {
@@ -169,6 +286,17 @@ class _megaPhoneFormState extends State<megaPhoneForm> {
                 ),
               );
               ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            } else if (_timeresultindex == null) {
+              final snackBar = SnackBar(
+                content: const Text('종료 시간을 설정해 주세요!'),
+                action: SnackBarAction(
+                  label: '확인',
+                  onPressed: () {
+                    // Some code to undo the change.
+                  },
+                ),
+              );
+              ScaffoldMessenger.of(context).showSnackBar(snackBar);
             } else {
               createMarker.newmegaphone['gatherText'] = myMegaText.text;
 
@@ -177,6 +305,21 @@ class _megaPhoneFormState extends State<megaPhoneForm> {
                   : null;
 
               createMarker.newmegaphone['userId'] = user.userinfo["userId"];
+
+              String finishdate = addtime();
+
+              // print(finishdate);
+              // print(finishdate.runtimeType);
+              // print(finishdate.toString());
+              // print(finishdate.toString().runtimeType);
+
+              var i = finishdate.lastIndexOf(':');
+              finishdate = finishdate.substring(0, i);
+              finishdate = finishdate.replaceFirst(' ', 'T');
+
+              createMarker.newmegaphone['gatherFinishdate'] = finishdate;
+
+              // createMarker.newmegaphone['gatherFinishdate'] = "2022-11-03T01:37";
 
               createMarker.file = file;
 
