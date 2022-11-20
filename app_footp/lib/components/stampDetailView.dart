a65194e8a0b400a8405699023e463f1c2557c1b0
김기태 <lop246@naver.com>
2022-11-20 14:00:35 +0900

    [FE/mainMap] ⚡feat : create map of stamp detail page

--- app_footp/lib/components/stampDetailView.dart
+++ app_footp/lib/components/stampDetailView.dart
@@ -1,6 +1,8 @@
+import 'dart:async';
 import 'package:app_footp/components/mainMap/stampList.dart';
 import 'package:app_footp/components/msgFoot/normalFoot.dart';
 import 'package:app_footp/custom_class/store_class/store.dart';
+import 'package:app_footp/mainMap.dart';
 import 'package:app_footp/myPage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
@@ -8,6 +10,7 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:get/get.dart';
 import 'package:dio/dio.dart' as DIO;
+import 'package:naver_map_plugin/naver_map_plugin.dart';
 
 class StampDetailView extends StatefulWidget {
   const StampDetailView({super.key});
@@ -20,7 +23,10 @@ class _StampDetailViewState extends State<StampDetailView> {
   // JoinStampInfo joinedStamp = Get.put(JoinStampInfo());
   StampMessage stampMessage = Get.put(StampMessage());
   UserData user = Get.put(UserData());
+  MyPosition myPosition = Get.put(MyPosition());
   JoinStampInfo joinedStamp = Get.put(JoinStampInfo());
+  Completer<NaverMapController> _controller = Completer();
+  List<Marker> markers = maindata.markers;
 
   @override
   void initState() {
@@ -34,82 +40,104 @@ class _StampDetailViewState extends State<StampDetailView> {
     print('===========================================');
   }
 
+  void _onMapCreated(NaverMapController controller) {
+    // if (!user.isLogin()) {
+    //   Navigator.push(
+    //     context,
+    //     MaterialPageRoute(builder: (context) => const SignIn()),
+    //   );
+    // }
+    if (_controller.isCompleted) _controller = Completer();
+    _controller.complete(controller);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Image.asset('imgs/logo.png', height: 45),
-          elevation: 0,
-          leading: BackButton(
-            color: Colors.black,
-          ),
-          backgroundColor: Colors.white,
-          centerTitle: true,
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(
-                Icons.account_circle,
-                color: Color.fromARGB(255, 153, 181, 229),
-                size: 40,
-              ),
-              padding: const EdgeInsets.only(top: 5, right: 20.0),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const MyPage()),
-                );
-              },
+      appBar: AppBar(
+        title: Image.asset('imgs/logo.png', height: 45),
+        elevation: 0,
+        leading: BackButton(
+          color: Colors.black,
+        ),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.account_circle,
+              color: Color.fromARGB(255, 153, 181, 229),
+              size: 40,
             ),
+            padding: const EdgeInsets.only(top: 5, right: 20.0),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const MyPage()),
+              );
+            },
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: (MediaQuery.of(context).size.height -
+                      MediaQuery.of(context).padding.top) *
+                  0.65,
+              child: NaverMap(
+                onMapCreated: _onMapCreated,
+                initialCameraPosition: CameraPosition(
+                    target: LatLng(
+                        stampMessage.stampMessage1["messageLatitude"],
+                        stampMessage.stampMessage1["messageLongitude"]),
+                    zoom: 18.0),
+                minZoom: 5.0,
+                locationButtonEnable: true,
+                markers: markers,
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(child: Text('제목')),
+            SizedBox(height: 10),
+            Container(child: Text(stampMessage.viewStamp["stampboard_title"])),
+            SizedBox(height: 10),
+            Container(child: Text('설명')),
+            SizedBox(height: 10),
+            Container(child: Text(stampMessage.viewStamp["stampboard_text"])),
+            SizedBox(height: 10),
+            NormalFoot(stampMessage.stampMessage1),
+            SizedBox(height: 10),
+            NormalFoot(stampMessage.stampMessage2),
+            SizedBox(height: 10),
+            NormalFoot(stampMessage.stampMessage3),
+            SizedBox(height: 10),
+            // Row(
+            //   children: [
+            //     joinedStamp.joinedStamp["stampboard_id"] != stampMessage.viewStamp
+            //         ? TextButton(
+            //             child: Text('참가하기'),
+            //             onPressed: () {
+            //               print(
+            //                   '###########################################');
+            //               print(joinedStamp.joinedStampId);
+            //               print(stampMessage.viewStamp);
+            //               print('######################################');
+            //               joinedStamp.joinedStampId = stampMessage.viewStamp;
+            //               print(joinedStamp.joinedStampId);
+            //             },
+            //           )
+            //         : TextButton(
+            //             child: Text('참가 취소',
+            //                 style: TextStyle(color: Colors.red)),
+            //             onPressed: () {},
+            //           ),
+            //   ],
+            // )
           ],
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                height: 400,
-                child: Center(
-                  child: Text('지도들어가요'),
-                ),
-              ),
-              Container(child: Text('제목')),
-              SizedBox(height: 10),
-              Container(
-                  child: Text(stampMessage.viewStamp["stampboard_title"])),
-              SizedBox(height: 10),
-              Container(child: Text('설명')),
-              SizedBox(height: 10),
-              Container(child: Text(stampMessage.viewStamp["stampboard_text"])),
-              SizedBox(height: 10),
-              NormalFoot(stampMessage.stampMessage1),
-              SizedBox(height: 10),
-              NormalFoot(stampMessage.stampMessage2),
-              SizedBox(height: 10),
-              NormalFoot(stampMessage.stampMessage3),
-              SizedBox(height: 10),
-              // Row(
-              //   children: [
-              //     joinedStamp.joinedStamp["stampboard_id"] != stampMessage.viewStamp
-              //         ? TextButton(
-              //             child: Text('참가하기'),
-              //             onPressed: () {
-              //               print(
-              //                   '###########################################');
-              //               print(joinedStamp.joinedStampId);
-              //               print(stampMessage.viewStamp);
-              //               print('######################################');
-              //               joinedStamp.joinedStampId = stampMessage.viewStamp;
-              //               print(joinedStamp.joinedStampId);
-              //             },
-              //           )
-              //         : TextButton(
-              //             child: Text('참가 취소',
-              //                 style: TextStyle(color: Colors.red)),
-              //             onPressed: () {},
-              //           ),
-              //   ],
-              // )
-            ],
-          ),
-        ));
+      ),
+    );
   }
 }
